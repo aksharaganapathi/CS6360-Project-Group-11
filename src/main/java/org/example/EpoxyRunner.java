08ddd5ac22312e20ca4e7fc326ea8e66372603b8
Akshara Ganapathi <ga385878@gmail.com>
2024-11-16 23:51:32 -0600

    Updated pom.xml

--- src/main/java/org/example/EpoxyRunner.java
+++ src/main/java/org/example/EpoxyRunner.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 
 public class EpoxyRunner {
     public static void main(String[] args) throws SQLException, InterruptedException {
+        System.out.println("Running Epoxy Benchmarks: ");
         HotelBenchmark benchmark = new HotelBenchmark();
         System.out.println("Running Hotel Benchmark (80% searches, 20% reservations)");
         benchmark.runBenchmark(10, 10000);
